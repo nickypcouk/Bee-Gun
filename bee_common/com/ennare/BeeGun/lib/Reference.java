7179f796f2d7da5dc1b353fd54795101e30995e7
nickypcouk <nicky@nickyp.co.uk>
2013-05-16 19:02:23 +0100

    End of Day 1

---
+++ bee_common/com/ennare/BeeGun/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.ennare.BeeGun.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "BeeGun";
+    public static final String MOD_NAME = "Bee Gun Mod";
+    public static final String VERSION = "0.0.1";
+}
---
+++ bee_common/com/ennare/Bee_Gun/BeeGun.java
@@ -0,0 +1,28 @@
+package com.ennare.Bee_Gun;
+
+import com.ennare.BeeGun.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION) 
+public class BeeGun {
+
+    @PreInit
+    public void PreInit(FMLPreInitializationEvent event) {
+        
+    }
+    @Init
+    public void Init (FMLInitializationEvent event) {
+        
+    }
+    @PostInit
+    public void PostInit (FMLPostInitializationEvent event) {
+        
+    }
+}
